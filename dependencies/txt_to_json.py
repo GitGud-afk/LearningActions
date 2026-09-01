e1e88d457e9f75153f2f4ce468b342c27fd23cd1
GitGud-afk <vinay.k3@tataelxsi.co.in>
2026-09-01 18:10:55 +0530

    Update txt_to_json.py
    fixed error in file

--- dependencies/txt_to_json.py
+++ dependencies/txt_to_json.py
@@ -1,17 +1,17 @@
 import json
 
 with open("input/inputTxt.txt") as f:
-  lines = f.read()
+  lines = f.readlines()
 
 result = dict()
 
 
-for line in lines:
-  splitLine = line.split(":")
-  key = splitLine[0].strip()
-  value = splitLine[1].strip()
-
-result[key] = value
+for line in lines:]
+  if ":" in line:
+    splitLine = line.split(":")
+    key = splitLine[0].strip()
+    value = splitLine[1].strip()
+    result[key] = value
 
 
 with open("intermediatesData/splitKey.json") as f:
