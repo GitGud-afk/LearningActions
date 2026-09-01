060ece4f83af928d70901f108ad795a3d0bf9f3c
GitGud-afk <vinay.k3@tataelxsi.co.in>
2026-09-01 17:17:26 +0530

    Create jsonToArtifacts.py
    created file to convert json to text

---
+++ dependencies/jsonToArtifacts.py
@@ -0,0 +1,12 @@
+import json
+
+with open("../intermediatesData/splitKey.json") as f:
+  myDict = json.load()
+
+lines = [f'For key "{key}", the value is "{value}".\n' for key, value in myDict.items()]
+
+
+
+with open("../output/formattedTxt.txt", 'w') as f:
+  f.write("\n".join(lines))
+  
