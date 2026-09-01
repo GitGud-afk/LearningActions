5ea45a5dc33daf37a84640a82cf8857b0d5c0fe5
GitGud-afk <vinay.k3@tataelxsi.co.in>
2026-09-01 18:22:01 +0530

    Update jsonToArtifacts.py
    fixed error in code

--- dependencies/jsonToArtifacts.py
+++ dependencies/jsonToArtifacts.py
@@ -1,12 +1,13 @@
 import json
+import os
 
 with open("intermediatesData/splitKey.json") as f:
-  myDict = json.load()
+  myDict = json.load(f)
 
 lines = [f'For key "{key}", the value is "{value}".\n' for key, value in myDict.items()]
 
 
-
+os.makedirs("output", exist_ok = True")
 with open("output/formattedTxt.txt", 'w') as f:
   f.write("\n".join(lines))
   
