7003a976731eeb4c38e82b8c57ac9f7b87b6a7c2
GitGud-afk <vinay.k3@tataelxsi.co.in>
2026-09-01 18:07:14 +0530

    Update jsonToArtifacts.py
    relative path fixed

--- dependencies/jsonToArtifacts.py
+++ dependencies/jsonToArtifacts.py
@@ -1,12 +1,12 @@
 import json
 
-with open("../intermediatesData/splitKey.json") as f:
+with open("intermediatesData/splitKey.json") as f:
   myDict = json.load()
 
 lines = [f'For key "{key}", the value is "{value}".\n' for key, value in myDict.items()]
 
 
 
-with open("../output/formattedTxt.txt", 'w') as f:
+with open("output/formattedTxt.txt", 'w') as f:
   f.write("\n".join(lines))
   
